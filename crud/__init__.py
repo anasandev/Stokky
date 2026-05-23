74865db72cca1e90f89b2bf06999427a224c25e9
Ana Leticia <ana.leticiaos1808@gmail.com>
2026-05-23 11:44:59 -0300

    feat: fluxo de empresa e relatorios

--- crud/__init__.py
+++ crud/__init__.py
@@ -1,5 +1,7 @@
 from .produto import cadastrar_produto, listar_produtos, atualizar_produto, deletar_produto
 from .estoque import listar_estoque, adicionar_estoque, remover_estoque
+from .empresa import (cadastrar_empresa, listar_empresas, obter_empresa_por_id, 
+                      atualizar_empresa, deletar_empresa)
 
 __all__ = [
     "cadastrar_produto",
@@ -9,4 +11,9 @@ __all__ = [
     "listar_estoque",
     "adicionar_estoque",
     "remover_estoque",
+    "cadastrar_empresa",
+    "listar_empresas",
+    "obter_empresa_por_id",
+    "atualizar_empresa",
+    "deletar_empresa",
 ]
